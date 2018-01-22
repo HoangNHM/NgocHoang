77535662544fc167d784b2bc48ece6b50c4417fc
Hoang Nguyen <HMHNguyen@absolute.com>
2018-01-23 05:37:39 +0700

    Data table with sorting, pagination, and filtering

---
+++ EMS-UI/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatTableModule,
+} from '@angular/material';
+
+
+import {AppComponent} from './app.component';
+import {SmsPageComponent} from './sms-page/sms-page.component';
+import {TableComponent} from './table/table.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SmsPageComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
